24186b0da3b3d7af0f101adfef26f43487fb922d
thago <cltchibinda30@gmail.com>
2023-02-14 09:05:03 +0100

    renaming signup component

---
+++ src/app/signuppage/signuppage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-uppage',
+  templateUrl: './signuppage.component.html',
+  styleUrls: ['./signuppage.component.scss']
+})
+export class SignUpComponent {
+
+}
